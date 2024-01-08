def98f36b3b31a75b62ed016198e52a964d2920e
unknown <victorsimasdev@gmail.com>
2024-01-08 11:49:16 -0300

    Created homepage and 'Add task' functionality

---
+++ todo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
+from todo.models import Task
 def home (request):
-    return HttpResponse('<h1>Home</h1>')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context={
+        'tasks': tasks,
+    }
+    return render(request, 'home.html', context)
     
