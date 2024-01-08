7e81801483e12de4b478e69e47fef22ab9d95fa6
unknown <victorsimasdev@gmail.com>
2024-01-08 12:26:55 -0300

    Added mark as done and undone

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
