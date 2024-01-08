c0301bba5575ea090190d58a10730959d159bef3
unknown <victorsimasdev@gmail.com>
2024-01-08 14:07:09 -0300

    Added edit function, created edit page

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -18,3 +18,16 @@ def mark_as_undone(request, pk):
     task.is_completed = False
     task.save()
     return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
